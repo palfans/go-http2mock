d41d7b2e4b24ec25ff4f54592993c1699a6c78ab
Zhu Can <can.zhu@hpe.com>
2021-03-30 23:34:37 -0500

    fix: split query and update responses

--- vui.go
+++ vui.go
@@ -11,19 +11,84 @@ import (
 )
 
 type vuiRequestStruct struct {
-	XMLName xml.Name      `xml:"VUI"`
-	VER     string        `xml:"ver,attr,omitempty"`
-	HDR     hdrStruct     `xml:"HDR,omitempty"`
-	Payload payloadStruct `xml:"Payload,omitempty"`
-	TrlRec  string        `xml:"TRL>REC,omitempty"`
+	XMLName xml.Name  `xml:"VUI"`
+	VER     string    `xml:"ver,attr,omitempty"`
+	HDR     hdrStruct `xml:"HDR,omitempty"`
+	Payload struct {
+		XMLName         xml.Name `xml:"Payload"`
+		AliQueryRequest struct {
+			XMLName     xml.Name `xml:"ALIQueryRequest"`
+			VER         string   `xml:"ver,attr,omitempty"`
+			ExternalKey string   `xml:"ExternalKey,omitempty"`
+		} `xml:"ALIQueryRequest"`
+		AliUpdateRequest struct {
+			XMLName         xml.Name `xml:"ALIUpdateRequest"`
+			FOC             string   `xml:"FOC,attr,omitempty"`
+			VER             string   `xml:"ver,attr,omitempty"`
+			ExternalKey     string   `xml:"ExternalKey,omitempty"`
+			ExternalKeyType string   `xml:"ExternalKeyType,omitempty"`
+			HNO             string   `xml:"HNO,omitempty"`
+			STN             string   `xml:"STN,omitempty"`
+			MCN             string   `xml:"MCN,omitempty"`
+			STA             string   `xml:"STA,omitempty"`
+			LOC             string   `xml:"LOC,omitempty"`
+			NAM             string   `xml:"NAM,omitempty"`
+			ClsType         string   `xml:"CLS>TYP,omitempty"`
+			TysType         string   `xml:"TYS>TYP,omitempty"`
+			COI             string   `xml:"COI,omitempty"`
+			CPF             string   `xml:"CPF,omitempty"`
+			ZIP             string   `xml:"ZIP,omitempty"`
+			SubscriberID    string   `xml:"SubscriberID,omitempty"`
+		} `xml:"ALIUpdateRequest"`
+	} `xml:"Payload,omitempty"`
+	TrlRec string `xml:"TRL>REC,omitempty"`
 }
 
-type vuiResponseStruct struct {
-	XMLName xml.Name      `xml:"VUI"`
-	VER     string        `xml:"ver,attr,omitempty"`
-	HDR     hdrStruct     `xml:"HDR,omitempty"`
-	Payload payloadStruct `xml:"Payload,omitempty"`
-	TrlRec  string        `xml:"TRL>REC,omitempty"`
+type vuiQueryResponseStruct struct {
+	XMLName xml.Name  `xml:"VUI"`
+	VER     string    `xml:"ver,attr,omitempty"`
+	HDR     hdrStruct `xml:"HDR,omitempty"`
+	Payload struct {
+		XMLName          xml.Name `xml:"Payload"`
+		AliQueryResponse struct {
+			XMLName     xml.Name  `xml:"ALIQueryResponse"`
+			VER         string    `xml:"ver,attr,omitempty"`
+			RC1         rc1Struct `xml:"RC1,omitempty"`
+			ExternalKey string    `xml:"ExternalKey,omitempty"`
+			HNO         string    `xml:"HNO,omitempty"`
+			STN         string    `xml:"STN,omitempty"`
+			MCN         string    `xml:"MCN,omitempty"`
+			STA         string    `xml:"STA,omitempty"`
+			LOC         string    `xml:"LOC,omitempty"`
+			NAM         string    `xml:"NAM,omitempty"`
+			ClsType     string    `xml:"CLS>TYP,omitempty"`
+			TysType     string    `xml:"TYS>TYP,omitempty"`
+			ESN         string    `xml:"ESN,omitempty"`
+			CPD         string    `xml:"CPD,omitempty"`
+			COI         string    `xml:"COI,omitempty"`
+			CPF         string    `xml:"CPF,omitempty"`
+			ZIP         string    `xml:"ZIP,omitempty"`
+			ALT         string    `xml:"ALT,omitempty"`
+		} `xml:"ALIQueryResponse"`
+	} `xml:"Payload,omitempty"`
+	TrlRec string `xml:"TRL>REC,omitempty"`
+}
+
+type vuiUpdateResponseStruct struct {
+	XMLName xml.Name  `xml:"VUI"`
+	VER     string    `xml:"ver,attr,omitempty"`
+	HDR     hdrStruct `xml:"HDR,omitempty"`
+	Payload struct {
+		XMLName           xml.Name `xml:"Payload"`
+		AliUpdateResponse struct {
+			XMLName         xml.Name  `xml:"ALIUpdateResponse"`
+			VER             string    `xml:"ver,attr,omitempty"`
+			ExternalKey     string    `xml:"ExternalKey,omitempty"`
+			ExternalKeyType string    `xml:"ExternalKeyType,omitempty"`
+			RC1             rc1Struct `xml:"RC1,omitempty"`
+		} `xml:"ALIUpdateResponse"`
+	} `xml:"Payload,omitempty"`
+	TrlRec string `xml:"TRL>REC,omitempty"`
 }
 
 type hdrStruct struct {
@@ -37,69 +102,6 @@ type rc1Struct struct {
 	XMLName xml.Name `xml:"RC1"`
 	RC1     string   `xml:",chardata"`
 	Message string   `xml:"message,attr,omitempty"`
-}
-
-type aliQueryRequestStruct struct {
-	XMLName     xml.Name `xml:"ALIQueryRequest"`
-	VER         string   `xml:"ver,attr,omitempty"`
-	ExternalKey string   `xml:"ExternalKey,omitempty"`
-}
-
-type aliQueryResponseStruct struct {
-	XMLName     xml.Name  `xml:"ALIQueryResponse"`
-	VER         string    `xml:"ver,attr,omitempty"`
-	RC1         rc1Struct `xml:"RC1,omitempty"`
-	ExternalKey string    `xml:"ExternalKey,omitempty"`
-	HNO         string    `xml:"HNO,omitempty"`
-	STN         string    `xml:"STN,omitempty"`
-	MCN         string    `xml:"MCN,omitempty"`
-	STA         string    `xml:"STA,omitempty"`
-	LOC         string    `xml:"LOC,omitempty"`
-	NAM         string    `xml:"NAM,omitempty"`
-	ClsType     string    `xml:"CLS>TYP,omitempty"`
-	TysType     string    `xml:"TYS>TYP,omitempty"`
-	ESN         string    `xml:"ESN,omitempty"`
-	CPD         string    `xml:"CPD,omitempty"`
-	COI         string    `xml:"COI,omitempty"`
-	CPF         string    `xml:"CPF,omitempty"`
-	ZIP         string    `xml:"ZIP,omitempty"`
-	ALT         string    `xml:"ALT,omitempty"`
-}
-
-type aliUpdateRequestStruct struct {
-	XMLName         xml.Name `xml:"ALIUpdateRequest"`
-	FOC             string   `xml:"FOC,attr,omitempty"`
-	VER             string   `xml:"ver,attr,omitempty"`
-	ExternalKey     string   `xml:"ExternalKey,omitempty"`
-	ExternalKeyType string   `xml:"ExternalKeyType,omitempty"`
-	HNO             string   `xml:"HNO,omitempty"`
-	STN             string   `xml:"STN,omitempty"`
-	MCN             string   `xml:"MCN,omitempty"`
-	STA             string   `xml:"STA,omitempty"`
-	LOC             string   `xml:"LOC,omitempty"`
-	NAM             string   `xml:"NAM,omitempty"`
-	ClsType         string   `xml:"CLS>TYP,omitempty"`
-	TysType         string   `xml:"TYS>TYP,omitempty"`
-	COI             string   `xml:"COI,omitempty"`
-	CPF             string   `xml:"CPF,omitempty"`
-	ZIP             string   `xml:"ZIP,omitempty"`
-	SubscriberID    string   `xml:"SubscriberID,omitempty"`
-}
-
-type aliUpdateResponseStruct struct {
-	XMLName         xml.Name  `xml:"ALIUpdateResponse"`
-	VER             string    `xml:"ver,attr,omitempty"`
-	ExternalKey     string    `xml:"ExternalKey,omitempty"`
-	ExternalKeyType string    `xml:"ExternalKeyType,omitempty"`
-	RC1             rc1Struct `xml:"RC1,omitempty"`
-}
-
-type payloadStruct struct {
-	XMLName           xml.Name                `xml:"Payload"`
-	AliQueryRequest   aliQueryRequestStruct   `xml:"ALIQueryRequest"`
-	AliQueryResponse  aliQueryResponseStruct  `xml:"ALIQueryResponse"`
-	AliUpdateRequest  aliUpdateRequestStruct  `xml:"ALIUpdateRequest,"`
-	AliUpdateResponse aliUpdateResponseStruct `xml:"ALIUpdateResponse"`
 }
 
 // VUI handler
@@ -116,60 +118,84 @@ func vuiHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Parse XML body
 	r.ParseForm()
-	vuiRequestBody := parseXMLRequestBody(r)
-	if vuiRequestBody == nil {
+	vuiRequestBody := parseRequestBody(r)
+	if vuiRequestBody.Payload.AliQueryRequest.ExternalKey != "" {
+		vuiQueryResponseBody, err := generateQueryResponse(vuiRequestBody)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Debug("VUI Request - ", http.StatusInternalServerError, " - ", "Internal Server Error")
+			return
+		} else {
+			vuiResponse(w, vuiQueryResponseBody)
+			return
+		}
+	} else if vuiRequestBody.Payload.AliUpdateRequest.ExternalKey != "" {
+		vuiUpdateResponseBody, err := generateUpdateResponse(vuiRequestBody)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Debug("VUI Request - ", http.StatusInternalServerError, " - ", "Internal Server Error")
+			return
+		} else {
+			vuiResponse(w, vuiUpdateResponseBody)
+			return
+		}
+	} else {
 		w.WriteHeader(http.StatusBadRequest)
 		log.Debug("VUI Request - ", http.StatusBadRequest, " - ", "Failed to parse XML request body")
 		return
 	}
 
-	vuiResponseBody, err := generateResponse(vuiRequestBody)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Debug("VUI Request - ", http.StatusInternalServerError, " - ", "Internal Server Error")
-		return
-	}
+}
+
+func vuiResponse(w http.ResponseWriter, vuiResponseBody []byte) {
 	w.Header().Set("Content-Type", "text/xml")
 	w.WriteHeader(http.StatusOK)
 	w.Write(vuiResponseBody)
 }
 
-func generateResponse(vuiRequestBody *vuiRequestStruct) ([]byte, error) {
-	vuiResponseBody := &vuiResponseStruct{}
-	if vuiRequestBody.Payload.AliQueryRequest.ExternalKey != "" {
-		vuiResponseBody.HDR.REC = "1"
-		vuiResponseBody.Payload.AliQueryResponse.ExternalKey = vuiRequestBody.Payload.AliQueryRequest.ExternalKey
-		vuiResponseBody.VER = "1.0"
-		vuiResponseBody.Payload.AliQueryResponse.VER = "1.0"
-		vuiResponseBody.Payload.AliQueryResponse.RC1.RC1 = "0"
-		vuiResponseBody.Payload.AliQueryResponse.RC1.Message = "SUCCESS"
-		vuiResponseBody.Payload.AliQueryResponse.HNO = "0000006080"
-		vuiResponseBody.Payload.AliQueryResponse.STN = "TENNYSON PKWY"
-		vuiResponseBody.Payload.AliQueryResponse.MCN = "PLANO"
-		vuiResponseBody.Payload.AliQueryResponse.STA = "TX"
-		vuiResponseBody.Payload.AliQueryResponse.LOC = "SUITE 400"
-		vuiResponseBody.Payload.AliQueryResponse.NAM = "ENTITLEMENT"
-		vuiResponseBody.Payload.AliQueryResponse.ClsType = "0"
-		vuiResponseBody.Payload.AliQueryResponse.TysType = "0"
-		vuiResponseBody.Payload.AliQueryResponse.ESN = "00888"
-		vuiResponseBody.Payload.AliQueryResponse.CPD = time.Now().Format("2006-01-02")
-		vuiResponseBody.Payload.AliQueryResponse.CPF = "HPE"
-		vuiResponseBody.Payload.AliQueryResponse.ZIP = "75024"
-		vuiResponseBody.Payload.AliQueryResponse.ALT = "0000000000"
-		vuiResponseBody.TrlRec = "1"
-	} else if vuiRequestBody.Payload.AliUpdateRequest.ExternalKey != "" {
-		vuiResponseBody.HDR.REC = "1"
-		vuiResponseBody.Payload.AliUpdateResponse.ExternalKey = vuiRequestBody.Payload.AliUpdateRequest.ExternalKey
-		vuiResponseBody.VER = "1.0"
-		vuiResponseBody.Payload.AliUpdateResponse.VER = "1.0"
-		vuiResponseBody.Payload.AliUpdateResponse.RC1.RC1 = "000"
-		vuiResponseBody.Payload.AliUpdateResponse.RC1.Message = "SUCCESS"
-		vuiResponseBody.TrlRec = "1"
-	}
+func generateQueryResponse(vuiRequestBody *vuiRequestStruct) ([]byte, error) {
+	vuiResponseBody := &vuiQueryResponseStruct{}
+
+	vuiResponseBody.HDR.REC = "1"
+	vuiResponseBody.Payload.AliQueryResponse.ExternalKey = vuiRequestBody.Payload.AliQueryRequest.ExternalKey
+	vuiResponseBody.VER = "1.0"
+	vuiResponseBody.Payload.AliQueryResponse.VER = "1.0"
+	vuiResponseBody.Payload.AliQueryResponse.RC1.RC1 = "0"
+	vuiResponseBody.Payload.AliQueryResponse.RC1.Message = "SUCCESS"
+	vuiResponseBody.Payload.AliQueryResponse.HNO = "0000006080"
+	vuiResponseBody.Payload.AliQueryResponse.STN = "TENNYSON PKWY"
+	vuiResponseBody.Payload.AliQueryResponse.MCN = "PLANO"
+	vuiResponseBody.Payload.AliQueryResponse.STA = "TX"
+	vuiResponseBody.Payload.AliQueryResponse.LOC = "SUITE 400"
+	vuiResponseBody.Payload.AliQueryResponse.NAM = "ENTITLEMENT"
+	vuiResponseBody.Payload.AliQueryResponse.ClsType = "F"
+	vuiResponseBody.Payload.AliQueryResponse.TysType = "0"
+	vuiResponseBody.Payload.AliQueryResponse.ESN = "00888"
+	vuiResponseBody.Payload.AliQueryResponse.CPD = time.Now().Format("2006-01-02")
+	vuiResponseBody.Payload.AliQueryResponse.CPF = "HPE"
+	vuiResponseBody.Payload.AliQueryResponse.ZIP = "75024"
+	vuiResponseBody.Payload.AliQueryResponse.ALT = "0000000000"
+	vuiResponseBody.TrlRec = "1"
+
 	return xml.MarshalIndent(vuiResponseBody, "", " ")
 }
 
-func parseXMLRequestBody(r *http.Request) *vuiRequestStruct {
+func generateUpdateResponse(vuiRequestBody *vuiRequestStruct) ([]byte, error) {
+	vuiResponseBody := &vuiUpdateResponseStruct{}
+
+	vuiResponseBody.HDR.REC = "1"
+	vuiResponseBody.Payload.AliUpdateResponse.ExternalKey = vuiRequestBody.Payload.AliUpdateRequest.ExternalKey
+	vuiResponseBody.Payload.AliUpdateResponse.ExternalKeyType = "OTHER"
+	vuiResponseBody.VER = "1.0"
+	vuiResponseBody.Payload.AliUpdateResponse.VER = "1.0"
+	vuiResponseBody.Payload.AliUpdateResponse.RC1.RC1 = "000"
+	vuiResponseBody.Payload.AliUpdateResponse.RC1.Message = "SUCCESS"
+	vuiResponseBody.TrlRec = "1"
+
+	return xml.MarshalIndent(vuiResponseBody, "", " ")
+}
+
+func parseRequestBody(r *http.Request) *vuiRequestStruct {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
